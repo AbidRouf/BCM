76ffe7e0151a3fdcd094d9f14e3e214f30e36b2e
Abid <arouf1471@gmail.com>
2022-01-19 08:02:05 +0000

    4th

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import NavLogo from "../assets/BlueCaveMedia.svg"
 
+
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px )`
+    }
+}
+
+
 const Team = () => {
     return (
 
-        <section id="Team">
+        <section id="Team" onMouseMove={moveBackground}>
 
             <div className="team__description">
 
